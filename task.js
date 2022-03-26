5133acebd9ded9d103ffddb19724bd59586f7ccc
MildDandy <37549166+MildDandy@users.noreply.github.com>
2022-03-26 02:00:01 -0400

    Fully transfered CSS and JavaScript to external files

---
+++ task.js
@@ -0,0 +1,55 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  //user can't click on submit yet
+  document.querySelector("#submit").disabled = true;
+  //Only enable when user types something
+  document.querySelector("#task").onkeyup = () => {
+    if(document.querySelector("#task").value.length > 0){
+      document.querySelector("#submit").disabled = false;
+    }
+    else{
+      document.querySelector("#submit").disabled = true;
+    }
+  }
+
+  document.querySelector("#new_task").onsubmit = () => {
+    //This is programatically creating a new element, referencing an empty <li></li> item
+    const li = document.createElement("li");
+    //Creating a string literal AND a button given a class called remove;
+    let task_text = document.querySelector("#task").value;
+    let new_task_html = `
+      <span>${task_text}</span>
+      <button class="remove">❌</button>
+      <button class="complete">✔️</button>
+    `;
+    li.innerHTML = new_task_html;
+    // Add the actual new item to the empty tasks_list
+    document.querySelector("#tasks_list").append(li);
+    //Doesn't exist in the DOM, exists locally in the browser in memory
+    //Resets the input field and submit button for the next go-around
+    document.querySelector("#task").value = "";
+    document.querySelector("#submit").disabled = true;
+    //Prevents the page from reloading
+    return false;
+  }
+
+
+
+
+  //Click event registered to the ENTIRE page.
+  document.addEventListener("click", event => {
+    console.log(event.target.className);
+    console.log(event.target.parentElement);
+    element = event.target;
+    if(element.className === "remove"){
+      element.parentElement.remove();
+    }
+
+    if(element.className === "complete"){
+      //element.getElementsByTagName("style")
+      element.parentElement.classList.add("completed");
+      element.remove();
+      //Make the element span text strikethrough
+    }
+  })
+})
